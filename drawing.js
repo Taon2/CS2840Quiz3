60f4d5ab48daf7f3762a068024996bed3899c059
Taon2 <cdoughtyse@gmail.com>
2021-01-07 09:31:29 -0600

    Add pen size tool

--- drawing.js
+++ drawing.js
@@ -9,6 +9,7 @@
 // See https://jshint.com/docs/ for more JSHint directives
 // jshint unused:false
 const default_color = "blue";
+let selected_size = "small";
 
 class Drawing {
     constructor() {
@@ -24,6 +25,12 @@ class Drawing {
             "purple",
         ];
 
+        const sizes = [
+            "small",
+            "medium",
+            "large",
+        ];
+
 // Initializes the control panel and adds it to the drawing
 //   Param - drawing: the drawing element
         const initControlPanel = (drawing) => {
@@ -35,6 +42,15 @@ class Drawing {
                 button.innerText = color;
                 button.onclick = () => {
                     drawing.selectedColor = color;
+                };
+                row.appendChild(button);
+            });
+
+            sizes.forEach((size) => {
+                const button = document.createElement("button");
+                button.innerText = size;
+                button.onclick = () => {
+                    selected_size = size;
                 };
                 row.appendChild(button);
             });
@@ -56,13 +72,14 @@ class Drawing {
         };
 
 // Initializes creates and initializes a new row
-        const initRow = () => {
+        const initRow = (id) => {
             const row = document.createElement("div");
             row.classList.add("row");
+            row.id = "row" + id;
             return row;
         };
 
-        this.onload = () => {
+        window.onload = () => {
             const drawings = document.getElementsByClassName("drawing");
             let cells = new Cell();
             // Loop through all drawings on the page
@@ -71,12 +88,12 @@ class Drawing {
 
                 // Add rows
                 for (let i = 0; i < rows; i++) {
-                    const row = initRow();
+                    const row = initRow(i);
                     drawing.appendChild(row);
 
                     // Create the cells and add them to the row
                     for (let j = 0; j < columns; j++) {
-                        row.appendChild(cells.initCell(drawing));
+                        row.appendChild(cells.initCell(drawing, j));
                     }
                 }
             })
@@ -88,9 +105,10 @@ class Cell {
     constructor() {
 // Initializes creates and initializes a new cell
 //   Param - drawing: the drawing that contains the cells
-        this.initCell = (drawing) => {
+        this.initCell = (drawing, id) => {
             const cell = document.createElement("div");
             cell.classList.add("cell");
+            cell.id = "cell" + id;
             cell.style.backgroundColor = default_color;
 
             // Remove onclick since onmousedown handles color flip
@@ -111,7 +129,40 @@ class Cell {
 //   Param - drawing: the drawing element
 //           cell: the cell to draw the color on
         this.doDraw = (drawing, cell) => {
-            cell.style.backgroundColor = drawing.selectedColor;
+            let rowId = cell.parentNode.id.substring(cell.parentNode.id.lastIndexOf("w") + 1) // gets the final "w" in row
+            let colId = cell.id.substring(cell.id.lastIndexOf("l") + 1) // gets the final "l" in cell
+
+            switch (selected_size) {
+                case "small":
+                    cell.style.backgroundColor = drawing.selectedColor;
+                    break
+                case "medium":
+                    for (let r = -1; r <= 1; r++) {
+                        let row = document.getElementById("row" + (rowId - r));
+                        for (let c = -1; c <= 1; c++) {
+                            for (let child of row.children) {
+                                if (child.id === "cell" + (colId - c)) {
+                                    child.style.backgroundColor = drawing.selectedColor;
+                                    break
+                                }
+                            }
+                        }
+                    }
+                    break
+                case "large":
+                    for (let r = -2; r <= 2; r++) {
+                        let row = document.getElementById("row" + (rowId - r));
+                        for (let c = -2; c <= 2; c++) {
+                            for (let child of row.children) {
+                                if (child.id === "cell" + (colId - c)) {
+                                    child.style.backgroundColor = drawing.selectedColor;
+                                    break
+                                }
+                            }
+                        }
+                    }
+                    break
+            }
         };
     }
 }
