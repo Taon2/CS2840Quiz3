046d56f1ba877ff5a41afa25dbf67c48ca6dfdb0
zijla <lohmiller47@gmail.com>
2021-01-07 08:59:07 -0600

    adjusted drawing.js to work from classes

--- drawing.js
+++ drawing.js
@@ -8,102 +8,110 @@
 // jshint devel:true
 // See https://jshint.com/docs/ for more JSHint directives
 // jshint unused:false
-
-const rows = 100;
-const columns = 100;
 const default_color = "blue";
 
-const colors = [
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "purple",
-];
+class Drawing {
+    constructor() {
+        const rows = 100;
+        const columns = 100;
 
-// Draws the color on the cell
-//   Param - drawing: the drawing element
-//           cell: the cell to draw the color on
-const doDraw = (drawing, cell) => {
-    cell.style.backgroundColor = drawing.selectedColor;
-};
+        const colors = [
+            "red",
+            "orange",
+            "yellow",
+            "green",
+            "blue",
+            "purple",
+        ];
 
 // Initializes the control panel and adds it to the drawing
 //   Param - drawing: the drawing element
-const initControlPanel = (drawing) => {
-    const row = initRow();
-    drawing.appendChild(row);
-    colors.forEach((color) => {
-        const button = document.createElement("button");
-        button.style.backgroundColor = color;
-        button.innerText = color;
-        button.onclick = () => {
-            drawing.selectedColor = color;
+        const initControlPanel = (drawing) => {
+            const row = initRow();
+            drawing.appendChild(row);
+            colors.forEach((color) => {
+                const button = document.createElement("button");
+                button.style.backgroundColor = color;
+                button.innerText = color;
+                button.onclick = () => {
+                    drawing.selectedColor = color;
+                };
+                row.appendChild(button);
+            });
+            drawing.appendChild(row);
         };
-        row.appendChild(button);
-    });
-    drawing.appendChild(row);
-};
 
 // Initializes the drawing and sets mouse events
 //   Param - drawing: the drawing element to init
-const initDrawing = (drawing) => {
-    drawing.ismousedown = false;
-    drawing.selectedColor = colors[0];
-    drawing.onmousedown = () => {
-        drawing.ismousedown = true;
-    };
-    drawing.onmouseup = () => {
-        drawing.ismousedown = false;
-    };
-    initControlPanel(drawing);
-};
+        const initDrawing = (drawing) => {
+            drawing.ismousedown = false;
+            drawing.selectedColor = colors[0];
+            drawing.onmousedown = () => {
+                drawing.ismousedown = true;
+            };
+            drawing.onmouseup = () => {
+                drawing.ismousedown = false;
+            };
+            initControlPanel(drawing);
+        };
 
 // Initializes creates and initializes a new row
-const initRow = () => {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    return row;
-};
+        const initRow = () => {
+            const row = document.createElement("div");
+            row.classList.add("row");
+            return row;
+        };
 
+        this.onload = () => {
+            const drawings = document.getElementsByClassName("drawing");
+            let cells = new Cell();
+            // Loop through all drawings on the page
+            Array.from(drawings).forEach((drawing) => {
+                initDrawing(drawing);
+
+                // Add rows
+                for (let i = 0; i < rows; i++) {
+                    const row = initRow();
+                    drawing.appendChild(row);
+
+                    // Create the cells and add them to the row
+                    for (let j = 0; j < columns; j++) {
+                        row.appendChild(cells.initCell(drawing));
+                    }
+                }
+            })
+        };
+    }
+}
+
+class Cell {
+    constructor() {
 // Initializes creates and initializes a new cell
 //   Param - drawing: the drawing that contains the cells
-const initCell = (drawing) => {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.style.backgroundColor = default_color;
+        this.initCell = (drawing) => {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.backgroundColor = default_color;
 
-    // Remove onclick since onmousedown handles color flip
-    // cell.onclick = () => { doDraw(drawing, cell); };
-    
-    cell.onmouseenter = () => {
-        if(drawing.ismousedown) {
-            doDraw(drawing, cell);
-        }
-    };
-    cell.onmousedown = () => {
-        doDraw(drawing, cell);
-    };
-    return cell;
-};
+            // Remove onclick since onmousedown handles color flip
+            // cell.onclick = () => { doDraw(drawing, cell); };
 
-window.onload = () => {
-    const drawings = document.getElementsByClassName("drawing");
+            cell.onmouseenter = () => {
+                if (drawing.ismousedown) {
+                    this.doDraw(drawing, cell);
+                }
+            };
+            cell.onmousedown = () => {
+                this.doDraw(drawing, cell);
+            };
+            return cell;
+        };
 
-    // Loop through all drawings on the page
-    Array.from(drawings).forEach((drawing) => {
-        initDrawing(drawing);
-
-        // Add rows
-        for(let i = 0; i < rows; i++) {
-            const row = initRow();
-            drawing.appendChild(row);
-
-            // Create the cells and add them to the row
-            for(let j = 0; j < columns; j++) {
-                row.appendChild(initCell(drawing));
-            }
-        }
-    })
-};
+        // Draws the color on the cell
+//   Param - drawing: the drawing element
+//           cell: the cell to draw the color on
+        this.doDraw = (drawing, cell) => {
+            cell.style.backgroundColor = drawing.selectedColor;
+        };
+    }
+}
